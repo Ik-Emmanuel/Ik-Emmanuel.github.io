6b426c18f16185ac0775135d50cf969df404d1cc
IK Emmanuel <47215749+Ik-Emmanuel@users.noreply.github.com>
2021-09-18 18:36:32 +0100

    Update data.js

--- projects/sub/musicapp/data.js
+++ projects/sub/musicapp/data.js
@@ -1,56 +1,242 @@
-let songs = [{
-        name: 'song 1',
-        path: 'assets/musics/Song 1.mp3',
-        artist: 'artist 1',
-        cover: 'assets/images/cover 1.png'
-    },
-    {
-        name: 'song 2',
-        path: 'assets/musics/Song 2.mp3',
-        artist: 'artist 2',
-        cover: 'assets/images/cover 2.png'
-    },
-    {
-        name: 'song 3',
-        path: 'assets/musics/Song 3.mp3',
-        artist: 'artist 3',
-        cover: 'assets/images/cover 3.png'
-    },
-    {
-        name: 'song 4',
-        path: 'assets/musics/Song 4.mp3',
-        artist: 'artist 4',
-        cover: 'assets/images/cover 4.png'
-    },
-    {
-        name: 'The Mic Song',
-        path: 'assets/musics/Song 5.mp3',
-        artist: 'artist 5',
-        cover: 'assets/images/cover 5.png'
-    },
-    {
-        name: 'song 6',
-        path: 'assets/musics/Song 6.mp3',
-        artist: 'artist 6',
-        cover: 'assets/images/cover 6.png'
-    },
-    {
-        name: 'song 7',
-        path: 'assets/musics/Song 7.mp3',
-        artist: 'artist 7',
-        cover: 'assets/images/cover 7.png'
-    },
-    {
-        name: 'song 8',
-        path: 'assets/musics/Song 8.mp3',
-        artist: 'artist 8',
-        cover: 'assets/images/cover 8.png'
-    },
+////////////////////////////// carousels ///////////////////
 
-    {
-        name: 'Baajo ',
-        path: 'assets/musics/bajo.mp3',
-        artist: 'Kwesi ft Joeboy',
-        cover: 'assets/images/joe.jpg'
-    },
-]
+const carousel = [...document.querySelectorAll('.carousel img')];
+
+let carouselImageIndex = 0;
+
+const changeCarousel = () => {
+    carousel[carouselImageIndex].classList.toggle('active');
+    if (carouselImageIndex >= carousel.length - 1) {
+        carouselImageIndex = 0;
+    } else {
+        carouselImageIndex++;
+    }
+    carousel[carouselImageIndex].classList.toggle('active');
+
+}
+
+setInterval(() => {
+    changeCarousel();
+}, 3000);
+
+
+
+//////////////////navigations 
+
+///////////////toggle music player
+
+
+const musicPlayerSection = document.querySelector('.music-player-section')
+
+let clickCount = 1;
+
+musicPlayerSection.addEventListener('click', () => {
+    if (clickCount >= 2) {
+        musicPlayerSection.classList.add('active');
+        clickCount = 1;
+        return;
+    }
+    clickCount++;
+    setTimeout(() => {
+        clickCount = 1;
+    }, 250);
+})
+
+/////////////////////////////// backfrom music player
+
+const backToHomeBtn = document.querySelector('.music-player-section .back-btn');
+
+backToHomeBtn.addEventListener('click', () => {
+    musicPlayerSection.classList.remove('active')
+})
+
+////// access playlist 
+const playlistSection = document.querySelector('.playlist');
+
+const navBtn = document.querySelector('.music-player-section .nav-btn');
+
+navBtn.addEventListener('click', () => {
+    playlistSection.classList.add('active');
+})
+
+/////// back from playlist to music player 
+
+const backToMusicPlayer = document.querySelector('.playlist .back-btn');
+backToMusicPlayer.addEventListener('click', () => {
+    playlistSection.classList.remove('active');
+})
+
+
+
+//////// navigation done /////////////////////////////////  
+
+
+//////////////////// music 
+
+let currentMusic = 0;
+
+const music = document.querySelector('#audio-source');
+
+const seekBar = document.querySelector('.music-seek-bar');
+const songName = document.querySelector('.current-song-name');
+const artistName = document.querySelector('.artist-name');
+const coverImage = document.querySelector('.cover');
+const currentMusicTime = document.querySelector('.current-time');
+const musicDuration = document.querySelector('.duration');
+
+const queue = [...document.querySelectorAll('.queue')];
+const playlistCards = [...document.querySelectorAll('.playlist-card')];
+
+
+
+///////////// select all buttons here 
+const forwardBtn = document.querySelector('i.fa-forward');
+const backwardBtn = document.querySelector('i.fa-backward');
+const playBtn = document.querySelector('i.fa-play');
+const pauseBtn = document.querySelector('i.fa-pause');
+const repeatBtn = document.querySelector('span.fa-redo');
+const volumeBtn = document.querySelector('span.fa-volume-up');
+const volumeSlider = document.querySelector('.volume-slider');
+
+
+// playBtn click event play and pause toggle  
+
+playBtn.addEventListener('click', () => {
+    music.play()
+    playBtn.classList.remove('active');
+    pauseBtn.classList.add('active');
+})
+
+pauseBtn.addEventListener('click', () => {
+    music.pause()
+    pauseBtn.classList.remove('active');
+    playBtn.classList.add('active');
+})
+
+
+
+//// function for setting up music 
+
+const setMusic = (i) => {
+    seekBar.value = 0;
+    let song = songs[i];
+    currentMusic = i;
+
+    music.src = song.path;
+    songName.innerHTML = song.name;
+    artistName.innerHTML = song.artist;
+    coverImage.src = song.cover
+
+    setTimeout(() => {
+        seekBar.max = music.duration;
+        musicDuration.innerHTML = formatTime(music.duration);
+    }, 300);
+    currentMusicTime.innerHTML = '00 : 00';
+    queue.forEach(item => item.classList.remove('active'));
+    queue[currentMusic].classList.add('active')
+
+
+}
+
+setMusic(0);
+
+
+/// format duration in 00 : 00 format
+
+const formatTime = (time) => {
+    let min = Math.floor(time / 60);
+    if (min < 10) {
+        min = `0` + min;
+
+    }
+
+    let sec = Math.floor(time % 60);
+    if (sec < 10) {
+        sec = `0` + sec;
+    }
+
+    return `${min} : ${sec}`;
+}
+
+
+//// seekbar events  and music repeat loop logic
+setInterval(() => {
+    seekBar.value = music.currentTime;
+    currentMusicTime.innerHTML = formatTime(music.currentTime)
+    if(Math.floor(music.currentTime) == Math.floor(seekBar.max)){
+        if(repeatBtn.className.includes('active')){
+            setMusic(currentMusic);
+            playBtn.click();
+
+        }else{
+            forwardBtn.click();
+        }
+    }
+}, 500);
+
+
+seekBar.addEventListener('change', ()=>{
+    music.currentTime = seekBar.value;
+})
+
+
+/////////// forward btn 
+
+forwardBtn.addEventListener('click', ()=>{
+    if(currentMusic >= songs.length - 1){
+        currentMusic = 0;
+    }
+    else{
+        currentMusic++;
+    }
+    setMusic(currentMusic);
+    playBtn.click();
+})
+
+
+//////backward
+backwardBtn.addEventListener('click', ()=>{
+    if(currentMusic <= 0){
+        currentMusic = songs.length - 1;
+    }
+    else{
+        currentMusic--;
+    }
+    setMusic(currentMusic);
+    playBtn.click();
+})
+
+
+//////////// repeat btn 
+repeatBtn.addEventListener('click', () =>{
+    repeatBtn.classList.toggle('active');
+})
+
+
+
+
+///////////// volume section
+volumeBtn.addEventListener('click', ()=>{
+volumeBtn.classList.toggle('active');
+volumeSlider.classList.toggle('active');
+})
+
+volumeSlider.addEventListener('input', ()=>{
+    music.volume = volumeSlider.value;
+})
+
+
+queue.forEach((item, i) => {
+    item.addEventListener('click', ()=> {
+        setMusic(i);
+        playBtn.click();
+    })
+})
+
+playlistCards.forEach((item) =>{
+    item.addEventListener('click', ()=>{
+        playlistSection.classList.add('active');
+
+    })
+
+})
